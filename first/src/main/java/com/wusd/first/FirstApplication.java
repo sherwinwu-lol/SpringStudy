79a6589e5d61136101ee92c71441c8737a486690
wsd <wsd5656126@foxmail.com>
2019-03-15 13:25:58 +0800

    greetings from spring boot

--- first/src/main/java/com/wusd/first/FirstApplication.java
+++ first/src/main/java/com/wusd/first/FirstApplication.java
@@ -1,7 +1,13 @@
 package com.wusd.first;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.applet.AppletContext;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class FirstApplication {
@@ -10,4 +16,15 @@ public class FirstApplication {
         SpringApplication.run(FirstApplication.class, args);
     }
 
+    // 查询Spring在启动的时候注入了哪些bean
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            Arrays.stream(beanNames).forEach(System.out::println);
+        };
+    }
 }
