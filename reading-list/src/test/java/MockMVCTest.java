2fb228570ef6bc27c70f19cbdaf05fc5963a7607
wusd <wsd5656126@foxmail.com>
2019-03-20 22:50:48 +0800

    安全校验

--- reading-list/src/test/java/MockMVCTest.java
+++ reading-list/src/test/java/MockMVCTest.java
@@ -1,20 +1,96 @@
-import com.wusd.readinglist.ReadingListApplicationTests;
+import com.wusd.readinglist.Book;
+import org.hamcrest.Matchers;
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.beans.SamePropertyValuesAs.samePropertyValuesAs;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.contains;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebAppConfiguration
 @SpringBootTest
 public class MockMVCTest {
+    // 注入WebApplicationContext
     @Autowired
+    private WebApplicationContext webContext;
+
     private MockMvc mockMvc;
 
     @Before
     public void setupMockMvc() {
+        // 设置MockMvc
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webContext)
+                .build();
+    }
+
+    @Test
+    public void homePage() throws Exception {
+        mockMvc.perform(get("/readingList"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("readingList"))
+                .andExpect(model().attributeExists("books"))
+                .andExpect(model().attribute("books", Matchers.is(Matchers.empty())));
+    }
+
+    /**
+     * 测试提交一本新书
+     *
+     * @throws Exception
+     */
+    @Test
+    public void postBook() throws Exception {
+        mockMvc.perform(post("/readingList")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("title", "BOOK TITLE")
+                .param("author", "BOOK AUTHOR")
+                .param("isbn", "1234567890")
+                .param("description", "DESCRIPTION"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/readingList"));
+
+        Book expectedBook = new Book();
+        expectedBook.setId(1L);
+        expectedBook.setReader("craig");
+        expectedBook.setTitle("BOOK TITLE");
+        expectedBook.setAuthor("BOOK AUTHOR");
+        expectedBook.setIsbn("1234567890");
+        expectedBook.setDescription("DESCRIPTION");
+
+        mockMvc.perform(get("/readingList"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("readingList"))
+                .andExpect(model().attributeExists("books"))
+                .andExpect(model().attribute("books", hasSize(1)));
+//                .andExpect(model().attribute("books", contains(samePropertyValuesAs(expectedBook))));
 
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
