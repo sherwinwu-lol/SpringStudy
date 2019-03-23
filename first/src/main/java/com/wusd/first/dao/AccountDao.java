cd180eee3e48dd6722bcc1d28ffee64c310db270
wusd <wsd5656126@foxmail.com>
2019-03-23 16:30:34 +0800

    -- delete first-design-pattern

--- first/src/main/java/com/wusd/first/dao/AccountDao.java
+++
@@ -1,9 +0,0 @@
-package com.wusd.first.dao;
-
-import com.wusd.first.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// 数据访问层, 通过编写一个继承自JpaRepository的接口就能完成数据访问, 其中包含了基本的单表查询方法
-// 注意: account对象名, 不是具体的表名, Integer是主键的类型, 一般为Integer或者long
-public interface AccountDao extends JpaRepository<Account, Integer> {
-}
--- mybatis/src/main/java/com/wusd/mybatis/entity/Account.java
+++
@@ -1,4 +0,0 @@
-package com.wusd.mybatis.entity;
-
-public class Account {
-}
