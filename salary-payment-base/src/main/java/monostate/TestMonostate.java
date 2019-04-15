7f633d5cda589311d91f8829cd0135cedbcac262
wsd <wsd5656126@foxmail.com>
2019-04-15 20:34:07 +0800

    monostate

---
+++ salary-payment-base/src/main/java/monostate/TestMonostate.java
@@ -0,0 +1,33 @@
+package monostate;
+
+import junit.framework.TestCase;
+
+public class TestMonostate extends TestCase {
+    public TestMonostate(String name) {
+        super(name);
+    }
+
+    /**
+     * 测试可以设置和获取一个对象成员变量x
+     */
+    public void testInstance() {
+        Monostate m = new Monostate();
+        for (int x = 0; x < 10; x++) {
+            m.setX(x);
+            assertEquals(x, m.getX());
+        }
+    }
+
+    /**
+     * 测试两个实例的表现是一样的
+     */
+    public void testInstancesBehaveAsOne() {
+        Monostate m1 = new Monostate();
+        Monostate m2 = new Monostate();
+
+        for (int x = 0; x < 10; x++) {
+            m1.setX(x);
+            assertEquals(x, m2.getX());
+        }
+    }
+}
